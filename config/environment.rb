18229e67a8a645de4034cc5abc670d07e100d191
hcac10 <hcaceres@caltech.edu>
2021-04-14 18:12:26 +0000

    added sendgrid user/pass

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailler::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
+  :user_name => ENV['app208135007@heroku.com'],
+  :password => ENV['hr2disw54986'],
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
