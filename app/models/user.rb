7d022bc4dda5abcc8ca7c6ed84c97cdd9d850a47
hcac10 <hcaceres@caltech.edu>
2021-04-19 21:31:10 +0000

    finished rolify gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
  class User < ApplicationRecord
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
-         
+  
+  rolify
+  
   def to_s
     email
   end
@@ -13,4 +16,18 @@
   end
   
   has_many :courses
-end
+  
+  after_create :assign_default_role
+
+  def assign_default_role
+     if User.count == 1
+       self.add_role(:admin) if self.roles.blank?
+       self.add_role(:teacher)
+       self.add_role(:student)
+     else
+       self.add_role(:student) if self.roles.blank?
+       self.add_role(:teacher) #if you want any user to be able to create own courses
+     end
+  end
+
+ end
